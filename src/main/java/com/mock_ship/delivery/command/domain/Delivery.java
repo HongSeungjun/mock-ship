950df35dd7281aae70a1fa3a73c332cbe26bd8f6
홍승준 <ghdgn22@gmail.com>
2025-02-03 21:54:23 +0900

    REF : Delivery domain refactor

--- src/main/java/com/mock_ship/delivery/command/domain/Delivery.java
+++ src/main/java/com/mock_ship/delivery/command/domain/Delivery.java
@@ -1,29 +1,29 @@
 package com.mock_ship.delivery.command.domain;
 
 import com.mock_ship.common.model.Address;
+import com.mock_ship.order.command.domain.OrderNo;
 import jakarta.persistence.*;
-import lombok.Builder;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "delivery")
-// TODO : 빌더 패턴이 나을것인가??
-//@Builder
 public class Delivery {
     @EmbeddedId
     private DeliveryNo deliveryNo;
 
-    // TODO : 객체 연관 관계
-    private String orderId;
+    @Embedded
+    private OrderNo orderNo;
 
     @Embedded
-    @AttributeOverrides({
+    @AttributeOverrides({ // 저장될 컬럼명 지정
             @AttributeOverride(name = "zipCode", column = @Column(name = "delivery_zip_code")),
             @AttributeOverride(name = "address1", column = @Column(name = "delivery_addr1")),
             @AttributeOverride(name = "address2", column = @Column(name = "delivery_addr2"))
     })
     private Address deliveryAddress;
+    @Column(name = "state")
+    @Enumerated(EnumType.STRING)
     private DeliveryStatus deliveryStatus;
     private LocalDateTime deliveryDate;
     @Embedded
--- src/main/java/com/mock_ship/delivery/command/domain/DeliveryStatus.java
+++ src/main/java/com/mock_ship/delivery/command/domain/DeliveryStatus.java
@@ -1,18 +1,6 @@
 package com.mock_ship.delivery.command.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public enum DeliveryStatus {
-    PENDING(0, "pending","배송 전"),
-    IN_TRANSIT(1, "in_transit","배송 중"),
-    DELIVERED(2, "delivered","배송 완료"),
-    CANCELLED(3, "cancelled","취소");
-
-    private Integer value;
-    private String name;
-    private String description;
+    PENDING, IN_TRANSIT, DELIVERED, CANCELLED
 
 }
